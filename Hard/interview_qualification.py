001a53cabc24098a694ad60e575e6195a2623738
philipwur <philipwur@hotmail.com>
2022-12-01 13:07:43 +0100

    1-12-22

--- Hard/interview_qualification.py
+++ Hard/interview_qualification.py
@@ -6,14 +6,14 @@ def interview(lst, tot):
     
     timelims = [5, 5, 10, 10, 15, 15, 20, 20]
     
+    if tot > 120 or len(lst) < 8:
+        return "disqualified"
+    
     for i, j in zip(lst, timelims):
         
         if i > j:
             return "disqualified"
         
-    if tot > 120 or len(lst) < 8:
-        return "disqualified"
-    
     return "qualified"
     
 
