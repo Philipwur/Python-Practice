d3bc3e0697be1d023828add6f84770be1491c793
philipwur <philipwur@hotmail.com>
2022-12-06 17:43:15 +0100

    06-12-22

---
+++ Hard/Disarium_number.py
@@ -0,0 +1,16 @@
+#%%
+
+#https://edabit.com/challenge/yvJbdkmKHvCNtcZy9
+
+def is_disarium(n):
+    
+    x = "{}".format(n)
+    y = 0
+    
+    for i in range(len(x)):
+        y += int(x[i]) ** (i + 1)
+    
+    if n == y:
+        return True
+    else:
+        return False
