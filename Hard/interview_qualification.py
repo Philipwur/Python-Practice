67e019e3d95ffafd5784b36de739f3f56c8c1951
philipwur <philipwur@hotmail.com>
2022-12-08 11:58:59 +0100

    08-12-22

--- Hard/interview_qualification.py
+++ Hard/interview_qualification.py
@@ -19,3 +19,28 @@ def interview(lst, tot):
 
 interview([5, 5, 10, 10, 15, 15, 20, 20], 130)
 
+
+#%%
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Define the potential energy function
+def potential_energy(coordinates):
+  x, y, z = coordinates
+  V = x**2 + y**2 + z**2
+  return V
+
+# Create a grid of coordinates
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(x, y)
+
+# Calculate the potential energy at each point on the grid
+Z = potential_energy((X, Y, 0))
+
+# Plot the potential energy surface
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(X, Y, Z, cmap='viridis')
+plt.show()
