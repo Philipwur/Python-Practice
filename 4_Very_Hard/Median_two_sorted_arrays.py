b7ca34a053307459ff7560fcd3ed68fc12076fbf
philipwur <philipwur@hotmail.com>
2022-12-14 10:50:52 +0100

    14-12-22

---
+++ 4_Very_Hard/Median_two_sorted_arrays.py
@@ -0,0 +1,12 @@
+#%%
+
+#unsolved
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+        median = 0
+        
+        comb = nums1 + nums2
+        
+        return median
